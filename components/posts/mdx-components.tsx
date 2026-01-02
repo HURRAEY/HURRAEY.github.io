6ec2392e2d82f4c864fa0d7dd44a460a925fcd66
HURRAEY <ajvl33333@gmail.com>
2026-01-02 23:35:02 +0900

    Remove mdx-components.tsx file, which contained exports for MDX components used in posts, streamlining component organization.

--- components/posts/mdx-components.tsx
+++
@@ -1,7 +0,0 @@
-// MDX 컴포넌트 설정
-// 포스트에서 import해서 사용할 수 있습니다
-// 예: import { Video, Callout } from '../components/mdx-components'
-
-export { Video, Callout, CodeBlock, Image, Tabs, Tab } from '../mdx';
-
-
